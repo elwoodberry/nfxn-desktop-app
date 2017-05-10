13da978182e431760b7e7a79a3e075a44e323a6e
Elwood Berry <me@elwoodberry.com>
2017-05-10 16:04:22 -0400

    updated main.js and index.html

--- main.js
+++ main.js
@@ -1,3 +1,57 @@
 /*
 NFXN: MAIN.JS
 */
+
+const {
+	app,
+	BrowserWindow
+} = require('electron');
+
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow(){
+
+	// Create browser window
+	win = new BrowserWindow(
+		{
+			width: 800,
+			height: 600,
+			icon: __dirname + "/img/brand-image.png"
+		}
+	);
+
+	// Load index.html
+	win.loadURL(url.format(
+		{
+			pathname: path.join(__dirname, "index.html"),
+			protocol: "file:",
+			slashes: true
+		}
+	));
+
+	// Open DEV Tools
+	// win.webContents.openDevTools();
+
+	// 
+	win.on("closed", () => 
+		{
+			win = null;
+		}
+	);
+}
+
+
+// Run Create Window Function
+app.on("ready", createWindow);
+
+// Quit when all windows are closed
+app.on('window-all-closed', () => 
+	{
+		if(process.platform !== 'darwin'){
+			app.quit();
+		}
+	}
+);
---
+++ renderer.js
@@ -0,0 +1,3 @@
+/*
+NFXN: RENDERER.JS
+*/
